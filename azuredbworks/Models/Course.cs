84e3033506880faad50ff5a3d6996d73e7d1e88f
Ayokunle <therealayokunle@gmail.com>
2023-10-31 12:14:59 +0100

    first commit

---
+++ azuredbworks/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace azuredbworks.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+
+        public string CourseName { get; set; }
+
+        public decimal Rating { get; set; }
+    }
+}
---
+++ azuredbworks/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using azuredbworks.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azuredbworks
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Ensure to add the services
+            services.AddMvc();
+            services.AddTransient<CourseService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // Ensure to map the controllers accordingly
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Course}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
